b0ac099f603ee325a001af0e21fece3a4561a4e8
Matteo <matnice23@gmail.com>
2025-03-24 19:48:09 +0100

    Setup_testing_tools
    Installed: React Testing Library, Vitest, v8, jest-dom and more

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
@@ -7,5 +8,10 @@ export default defineConfig({
   plugins: [react(), svgr()],
   server: {
     port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
   },
 });
